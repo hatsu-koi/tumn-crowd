e370b1237336aca24874cd2a17e82472bd00d248
Khinenw <deu07115@gmail.com>
2018-08-29 19:10:09 +0900

    Updated dataset generation

--- export.mjs
+++ export.mjs
@@ -35,5 +35,40 @@ import mongodb from "mongodb";
 		};
 	})));
 
+	fs.writeFileSync('./result/multilabel.json', JSON.stringify(sentences.map(({content, filter}) => {
+		return {
+			content,
+			filter: filter.map(v => [v % 2 >= 1, v % 4 >= 2, v >= 4])
+		};
+	})));
+
+	const dataList = {};
+	['mature', 'swearwords', 'hatespeech'].forEach((v, i) => {
+		dataList[v] = {};
+
+		const mask = Math.pow(2, i);
+		sentences.forEach(sentence => {
+			sentence['content'].forEach((word, wi) => {
+				if(sentence['filter'][wi] & mask) {
+					dataList[v][word] = true;
+				}
+			});
+		});
+	});
+
+	fs.writeFileSync('./result/words.json', JSON.stringify(dataList, null, '\t'));
+
+	let sentencesEmbedding = await collection.find({
+		filter: {
+			$exists: true
+		}
+	}).toArray();
+
+	fs.writeFileSync('./result/embedding.json', JSON.stringify(sentencesEmbedding.map(({content}) => {
+		return {
+			content
+		};
+	})));
+
 	console.log("Finished!");
 })();
